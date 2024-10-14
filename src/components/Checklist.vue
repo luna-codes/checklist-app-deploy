25b71c8313a163e78e7d7209bca4f12149524635
luna-codes <joana.bartholo@gmail.com>
2024-10-14 19:07:47 -0300

    1st app commit

---
+++ src/components/Checklist.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Checklist</h1>
+    <input v-model="newItem" @keyup.enter="addItem" placeholder="Add a new item" />
+    <ul>
+      <ChecklistItem
+        v-for="(item, index) in items"
+        :key="index"
+        :item="item"
+        @remove="removeItem(index)"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ChecklistItem from './ChecklistItem.vue';
+
+export default {
+  components: {
+    ChecklistItem,
+  },
+  data() {
+    return {
+      newItem: '',
+      items: [],
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim()) {
+        this.items.push({ text: this.newItem, completed: false });
+        this.newItem = '';
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
---
+++ src/components/ChecklistItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <input type="checkbox" v-model="item.completed" />
+    <span :class="{ completed: item.completed }">{{ item.text }}</span>
+    <button @click="$emit('remove')">Remove</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
+
+<style>
+.completed {
+  text-decoration: line-through;
+}
+</style>
